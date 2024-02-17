eaa7558b31a87d00fbc5e451b380effff5d7bada
sseung12 <pon02129@naver.com>
2024-02-17 12:50:35 +0900

    init

---
+++ src/main/java/com/ss/queryoptimize/QueryOptimizeApplication.java
@@ -0,0 +1,13 @@
+package com.ss.queryoptimize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryOptimizeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueryOptimizeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ss/queryoptimize/QueryOptimizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ss.queryoptimize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryOptimizeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
